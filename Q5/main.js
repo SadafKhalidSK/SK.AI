7fce5d0ce0eea74d616e41c9f68df6af855a7dc2
SadafKhalidSK <148052407+SadafKhalidSK@users.noreply.github.com>
2024-03-15 02:16:05 -0700

    Add files via upload
    qeustion 5 of 45 assignment

---
+++ Q5/main.js
@@ -0,0 +1,5 @@
+"use strict";
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+let famous_person = "Eleanor Roosevelt";
+let message = "The future belongs to those who believe in the beauty of their dreams.";
+console.log(`"${famous_person} once said,${message}"`);
