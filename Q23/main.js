92b0f2ffac67d8f5b326d3077fbf08edb9687abd
SadafKhalidSK <148052407+SadafKhalidSK@users.noreply.github.com>
2024-03-15 02:24:12 -0700

    Add files via upload

---
+++ Q23/main.js
@@ -0,0 +1,48 @@
+"use strict";
+//Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. 
+//Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+let season = 'Winter';
+console.log("Is season == 'Winter'? I predict True.");
+console.log(season == 'Winter');
+/// test 2
+let a = '5', b = 10;
+console.log("b is equal to 10? I predict True.");
+console.log(b == 10);
+//  TEST N O3
+let a = '5', b = '10 ';
+console.log("a is equal to 5? I predict True.");
+console.log(a == '5');
+// TEST NO 4
+let a = 5, b = 10;
+console.log("a is not equal to 7?  predict True.");
+console.log(a != 7);
+// TEST NO 5 
+let a = 5, b = 10;
+console.log("b is greater than a?  predict True.");
+console.log(b > 5);
+//...............................................FALSE..............
+//TEST NO 1
+let season2 = 'Winter';
+console.log("Is season == 'Winter'? I predict True.");
+console.log(season2 == 'Summer');
+//TEST NO 2 
+let a = '5', b = 10;
+console.log("b is equal to a? I predict True.");
+console.log(b == a);
+//  TEST N O3
+let a = '5', b = '10 ';
+console.log("a is equal to 7? I predict True.");
+console.log(a == '7');
+// TEST NO 4
+let a = 5, b = 10;
+console.log("a is equal to 5?  predict True.");
+console.log(a != 5);
+// TEST NO 5 
+let a = 5, b = 10;
+console.log("b is greater than a?  predict True.");
+console.log(b < 5);
