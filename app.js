17e4b04f6d5f4b577c3a1932f0edc51ed9e5a908
SadafKhalidSK <148052407+SadafKhalidSK@users.noreply.github.com>
2024-03-15 02:04:46 -0700

    Add files via upload
    coding of upper ,lower and title case

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+// Q3.Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let personName = "ahmed";
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1));
---
+++ app.ts
@@ -0,0 +1,5 @@
+// Q3.Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let personName: string="ahmed"
+console.log(personName.toLowerCase())
+console.log(personName.toUpperCase())
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1))
