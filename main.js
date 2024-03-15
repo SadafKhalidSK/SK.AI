b63a234120cbf3dc921acea6aca96446ad07c000
SadafKhalidSK <148052407+SadafKhalidSK@users.noreply.github.com>
2024-03-15 02:09:23 -0700

    Add files via upload
    famous quote coding

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Q4.Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+// The future belongs to those who believe in the beauty of their dreams. -Eleanor Roosevelt
+let author = "Eleanor Roosevelt";
+let quote = "The future belongs to those who believe in the beauty of their dreams.";
+console.log(`${author} once said, "${quote}"`);
---
+++ main.ts
@@ -0,0 +1,8 @@
+// Q4.Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+// The future belongs to those who believe in the beauty of their dreams. -Eleanor Roosevelt
+let author:string="Eleanor Roosevelt";
+let quote="The future belongs to those who believe in the beauty of their dreams.";
+console.log(`${author} once said, "${quote}"`);
+
