30f8b43495d9c30e228a450d7aa2bf42266166da
SadafKhalidSK <148052407+SadafKhalidSK@users.noreply.github.com>
2024-03-15 02:27:03 -0700

    Add files via upload

---
+++ Q30/main.js
@@ -0,0 +1,14 @@
+//Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
+// • If the username is 'admin', print a special greeting, such as Hello admin,
+//  would you like to see a status report?
+// • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging 
+// in again.
+var usernames = ['user1', "admin", 'user2', 'user3', 'admin'];
+usernames.forEach(function (usernamess) {
+    if (usernamess === "admin") {
+        console.log("'Hello ADMIN,\n     would you like to see a status report?'");
+    }
+    else {
+        console.log("'Hello ".concat(usernamess, " ,thank you for logging \n         in again.'"));
+    }
+});
