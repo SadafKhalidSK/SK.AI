6e359d0fd736f12fd6cc682bfdab095d543c0c75
SadafKhalidSK <148052407+SadafKhalidSK@users.noreply.github.com>
2024-03-15 03:15:05 -0700

    Add files via upload

---
+++ QS31/main.js
@@ -0,0 +1,24 @@
+//Qs31 No Users: Add an if test to Exercise 28 to make sure the list of users 
+//is not empty.
+//• If the list is empty, print the message We need to find some users!
+//• Remove all of the usernames from your array, and make sure the correct 
+//message is printed.
+var usernames = ['shayan', 'simra', 'seniya', 'khalid'];
+if (usernames.length === 0) {
+    console.log('We need to find some users!');
+}
+else {
+    for (var i = 0; i > usernames.length; i++)
+        ;
+}
+console.log(usernames);
+console.log("removing all of the username form list");
+var usernamess = [];
+if (usernames.length === 0) {
+    console.log('We need to find some users!');
+}
+else {
+    for (var i = 0; i > usernamess.length; i++)
+        ;
+}
+console.log(usernamess);
---
+++ QS31/main.ts
@@ -0,0 +1,23 @@
+//Qs31 No Users: Add an if test to Exercise 28 to make sure the list of users 
+//is not empty.
+//• If the list is empty, print the message We need to find some users!
+
+//• Remove all of the usernames from your array, and make sure the correct 
+//message is printed.
+ let usernames:string[]=['shayan','simra','seniya','khalid']
+ if (usernames.length===0){
+    console.log('We need to find some users!')}
+    else{
+        for (let i=0;i>usernames.length; i++);
+    }
+console.log(usernames)
+
+
+console.log("removing all of the username form list")
+let usernamess:string[]=[]
+ if (usernames.length===0){
+    console.log('We need to find some users!')}
+    else{
+        for (let i=0;i>usernamess.length; i++);
+    }
+console.log(usernamess)
